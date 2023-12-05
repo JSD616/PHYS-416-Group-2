a096a8294a93dc3978ce4690fb3b83e0e42cd83a
JSD616 <102917398+JSD616@users.noreply.github.com>
2023-12-05 11:05:26 -0500

    Update XYWolff.m
    Turns out that adding a 1 fixed everything! Also now returns the map of cluster indices over the lattice as the 4th output

--- XYWolff.m
+++ XYWolff.m
@@ -1,26 +1,25 @@
 % Single iteration of Wolff algorithm on a XY-model square spin lattice
 % with size L, temperature kT, nearest-neighbor Hamiltonian
-% E = -J*sum(i,j nn) cos(θ_i - θ_j), n iterations
+% E = -J*sum(i,j nn) cos(θ_i - θ_j)
+% mode: 0 for toroidal, 1 for non-toroidal
 % 
-% Each iteration selects a uniformly random angle θ,
-% then checks each pair of neighboring spins to see if
-% they are on the "same side" of the unit vector at angle θ
+% Each iteration selects a uniformly random angle θ, corresponding
+% to the normal vector of the random "Wolff plane."
+% We then check each pair of neighboring spins to see if
+% they are on the "same side" of the Wolff plane
 % (i.e. cos(θi - θ) * cos(θj - θ) is positive). If so, the
 % bond between them is activated with probability
 % p = 1 - exp(-2j/kT * cos(θi - θ)*cos(θj - θ)), which
 % defines a graph over the lattice. 
 % The connected components of this graph are called "clusters,"
-% and each cluster is reflected across the chosen angle with
+% and each cluster is reflected across the Wolff plane with
 % probability 1/2.
-%
-% (Toroidal boundary conditions have not been implemented yet)
-% mode: 0 for toroidal, 1 for non-toroidal
 
 
-function [spin, X, Y] = XYWolff(spin, kT, j, L, mode)
+function [spin, X, Y, clusterMap] = XYWolff(spin, kT, j, L, mode)
     assert((mode == 0) || (mode == 1), 'mode value can only be 0 or 1.');
     theta = 2*rand;
-    A = latticeAdjacencyMatrix(L,L);
+    %A = latticeAdjacencyMatrix(L,L);
     B = zeros(L^2); % Stores bond activation states
     for x = 1:(L-mode)
         for y = 1:(L-mode)
@@ -60,10 +59,12 @@ function [spin, X, Y] = XYWolff(spin, kT, j, L, mode)
     % Effectively assign a coin flip to each cluster
     p = rand(max(clusters),1);
     
+    clusterMap = zeros(L,L);
     for ind = 1:L^2
+        [row, col] = ind2sub(size(spin),ind);
+        clusterMap(row,col) = clusters(ind);
         if p(clusters(ind)) <= 0.5
-            [row, col] = ind2sub(size(spin),ind);
-            spin(row, col) = mod(2*theta - spin(row, col), 2);
+            spin(row, col) = mod(2*theta + 1 - spin(row, col), 2);
         end
     end
     
